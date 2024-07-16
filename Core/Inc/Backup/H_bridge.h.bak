63914bec01844b40f6a6429d644b205b3c8d738a
JoshWilbur <joshuawilbur03@gmail.com>
2024-07-15 22:17:05 -0400

    H-Bridge state program added, allows clockwise/coclockwise motor rotation

---
+++ Core/Inc/Backup/H_bridge.h.bak
@@ -0,0 +1,3 @@
+// Header file for the H_bridge.c program
+void HBridge_State(int input, int Backward_LB, int Backward_UB, int Forward_LB, int Forward_UB);
+
---
+++ Core/Src/Backup/H_bridge.c
@@ -0,0 +1,23 @@
+// This file contains functions which govern the behavior of the H bridge circuit
+#include "main.h"
+// Function prototypes
+void HBridge_State(int input, int Backward_LB, int Backward_UB, int Forward_LB, int Forward_UB);
+
+// This function will use input from the potentiometer to determine the H-Bridge circuit state
+// For now, lets say the input ranges from 0 to 65535
+void HBridge_State(int input, int Backward_LB, int Backward_UB, int Forward_LB, int Forward_UB){
+
+	if(input >= Backward_LB && input <= Backward_UB){
+		TIM3->CCR1 = 20000-input;
+		TIM3->CCR2 = 0;
+		TIM3->CCR3 = 0;
+		TIM3->CCR4 = 20000-input;
+	}
+
+	if(input >= Forward_LB && input <= Forward_UB){
+		TIM3->CCR1 = 0;
+		TIM3->CCR2 = input-20000;
+		TIM3->CCR3 = input-20000;
+		TIM3->CCR4 = 0;
+	}
+}
