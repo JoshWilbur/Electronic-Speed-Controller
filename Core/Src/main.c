4ba73581ed4721bc1e233c6b87c51194fef49a70
simbomir <amir.seidakhmetov@maine.edu>
2024-12-05 01:08:58 -0500

    Code to make 7-segment display work

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -8,6 +8,8 @@
 #include "H_bridge.h"
 #include "user_input.h"
 #include "rpm_feedback.h"
+#include "stm32l4xx_hal.h"
+#include "7_segment_display.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -31,6 +33,8 @@ TIM_HandleTypeDef htim3;
 ADC_HandleTypeDef hadc1;
 ADC_HandleTypeDef hadc2;
 
+I2C_HandleTypeDef hi2c1;
+
 TIM_HandleTypeDef htim2;
 TIM_HandleTypeDef htim3;
 
@@ -44,6 +48,7 @@ static void MX_TIM3_Init(void);
 static void MX_ADC1_Init(void);
 static void MX_TIM2_Init(void);
 static void MX_ADC2_Init(void);
+static void MX_I2C1_Init(void);
 /* USER CODE BEGIN PFP */
 /* USER CODE END PFP */
 
@@ -57,6 +62,79 @@ int feedback_pot = 0;
 int exp_rpm;
 int real_rpm = 0;
 int feedback = 0;
+
+#define HT16K33_ADDRESS 0x70 << 1 // Shift left for HAL I2C
+uint8_t digitToSegment[] = {
+    0x3F, // 0
+    0x06, // 1
+    0x5B, // 2
+    0x4F, // 3
+    0x66, // 4
+    0x6D, // 5
+    0x7D, // 6
+    0x07, // 7
+    0x7F, // 8
+    0x6F  // 9
+};
+
+void HT16K33_Init() {
+    uint8_t command;
+
+    // Turn on the oscillator
+    command = 0x21; // System setup command
+    HAL_I2C_Master_Transmit(&hi2c1, HT16K33_ADDRESS, &command, 1, HAL_MAX_DELAY);
+
+    // Set brightness (e.g., medium brightness level)
+    command = 0xE7; // Brightness 7/15
+    HAL_I2C_Master_Transmit(&hi2c1, HT16K33_ADDRESS, &command, 1, HAL_MAX_DELAY);
+
+    // Enable display
+    command = 0x81; // Display on, no blinking
+    HAL_I2C_Master_Transmit(&hi2c1, HT16K33_ADDRESS, &command, 1, HAL_MAX_DELAY);
+}
+
+
+
+
+// Function to display an integer (max 4 digits)
+void HT16K33_DisplayInteger(int number) {
+    uint8_t displayBuffer[10] = {0}; // Initialize all positions to 0 (clear display)
+    int position;
+
+    // Handle the case when number is 0 (to avoid blank display)
+    if (number == 0) {
+        displayBuffer[8] = digitToSegment[0]; // Right-most position for "0"
+    } else {
+        // Extract digits into an array
+        int digits[4] = {0}; // Array to hold up to 4 digits
+        int count = 0;
+
+        while (number > 0 && count < 4) {
+            digits[count++] = number % 10; // Extract digits in reverse order
+            number /= 10;
+        }
+
+        // Map the digits to the correct display buffer indices
+        for (position = 0; position < count; position++) {
+            int bufferIndex;
+
+            // Determine the correct buffer index based on position
+            switch (position) {
+                case 0: bufferIndex = 8; break; // Right-most digit
+                case 1: bufferIndex = 6; break; // Third digit
+                case 2: bufferIndex = 2; break; // Second digit
+                case 3: bufferIndex = 0; break; // Left-most digit
+                default: continue; // Should not happen for 4 digits
+            }
+
+            displayBuffer[bufferIndex] = digitToSegment[digits[position]];
+        }
+    }
+
+    // Send the updated buffer to the HT16K33
+    HAL_I2C_Mem_Write(&hi2c1, HT16K33_ADDRESS, 0x00, I2C_MEMADD_SIZE_8BIT, displayBuffer, 10, HAL_MAX_DELAY);
+}
+
 /* USER CODE END 0 */
 
 /**
@@ -89,6 +167,7 @@ int main(void)
   MX_ADC1_Init();
   MX_TIM2_Init();
   MX_ADC2_Init();
+  MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
   // Start clocks for PWM and feedback timing
   HAL_TIM_Base_Start_IT(&htim2); // Begin TIM2 in interrupt mode
@@ -96,6 +175,8 @@ int main(void)
   HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
   HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_3);
   HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_4);
+
+  HT16K33_Init();
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -156,6 +237,8 @@ int main(void)
 		  exp_rpm = input_to_rpm(input_pot);
 		  feedback = closed_loop_feedback(exp_rpm, real_rpm);
 	  }
+
+	  HT16K33_DisplayInteger(input_pot);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -335,6 +418,54 @@ static void MX_ADC2_Init(void)
   /* USER CODE BEGIN ADC2_Init 2 */
 
   /* USER CODE END ADC2_Init 2 */
+
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.Timing = 0x10C18DCC;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Analogue filter
+  */
+  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Digital filter
+  */
+  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
 
 }
 
